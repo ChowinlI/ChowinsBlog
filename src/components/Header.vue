3c94abfa8dc5392742d765a225237d3a5f463878
ChowinlI <1983451555@q.com>
2018-10-26 18:26:45 +0800

    页面配色

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
     <header id="header">
       <div class="pf-wrap" :style="{ backgroundImage: 'url('+banner+')'}">
         <div class="pf-picture">
-          <div class="pf-picture-wrap"><img src="../assets/images/pf-photo.jpg" alt=""></div>
+          <div class="pf-picture-wrap"><img :src="avatar" alt=""></div>
         </div>
         <div class="pf-name"><span class="name">Chowin</span></div>
         <div class="pf-intro">一个懂得挖掘生活中的乐趣的Web前端爱好者,兼段子手</div>
@@ -13,18 +13,22 @@
           <router-link class="nav-list-item" to="/roast">吐槽</router-link>
           <router-link class="nav-list-item" to="/friends">dalao</router-link>
           <router-link class="nav-list-item" to="/about">关于我</router-link>
-          <div class="nav-list-item"><i class="iconfont icon-search"></i></div>
+          <!--<div class="nav-list-item"><i class="iconfont icon-search"></i></div>-->
         </div>
       </nav>
-      <!--<div class="header-bg" :style="{ backgroundImage: 'url('+banner+')'}"></div>-->
+      <div class="contact">
+        <div class="contact-item"><a href="javascript:void (0)"><img src="../../static/images/icon-github.png" alt=""></a></div>
+        <div class="contact-item"><a href="javascript:void (0)"><img src="../../static/images/icon-wechat.png" alt=""></a></div>
+        <div class="contact-item"><a href="javascript:void (0)"><img src="../../static/images/icon-sina.png" alt=""></a></div>
+      </div>
     </header>
 </template>
 
 <script type="text/ecmascript-6">
     $(function () {
       var h = $('#header').height();
-      $(window).on("scroll", function(){
-        var s_top = $(document).scrollTop();
+      $('#app').on("scroll", function(){
+        var s_top = $('#app').scrollTop();
         if(s_top >= h){
           $('.nav').addClass('nav-scroll');
           $('.back-top').fadeIn(500);
@@ -42,7 +46,8 @@
         props: [],
         data(){
             return {
-                banner: require('../assets/images/banner.jpg')
+                banner: require('../assets/images/bg2.jpg'),
+                avatar: require('../assets/images/pf-photo2.jpg')
             }
         },
         created(){
